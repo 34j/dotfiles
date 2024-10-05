b18a5db5b2682d68b603153c115a20654030230f
34j <55338215+34j@users.noreply.github.com>
2024-10-05 21:18:14 +0900

    chore: wip

--- main.bash
+++ main.bash
@@ -9,17 +9,18 @@ git config --global user.name "$user"
 git config --global user.email "$email"
 
 echo "Installing ghq..."
+sudo add-apt-repository ppa:longsleep/golang-backports -y
+sudo apt update
 sudo apt install golang-go -y
+echo "export PATH=\$PATH:$HOME/go/bin" >> ~/.bashrc
 go install github.com/x-motemen/ghq@latest
 
 echo "Installing GitHub CLI..."
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+# https://github.com/cli/cli/issues/4351#issuecomment-1631676133
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
 
 echo "Installing Python..."
 sudo apt install software-properties-common -y
@@ -110,12 +111,13 @@ echo "Please manually set the shortcut for diodon in "settings/keyboard/keyboard
 
 # this conflicts with cinnamon
 # echo "Installing xbindkeys..."
-# sudo apt install xbindkeys -yv
-# xbindkeys --defaults > ~/.xbindkeysrcv
+# sudo apt install xbindkeys -y
+# xbindkeys --defaults > ~/.xbindkeysrc
 # echo '"/usr/bin/diodon" ' >> ~/.xbindkeysrc
 # echo "  Mod4 + v" >> ~/.xbindkeysrc
 # echo '"gnome-terminal" ' >> ~/.xbindkeysrc
 # echo "  shift+F12 " >> ~/.xbindkeysrc
 
-echo "Logging in to GitHub..."vvvvv
+echo "Logging in to GitHub..."
 gh auth login --web
+gh auth setup-git
